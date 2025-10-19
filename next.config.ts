7a7492bbe4ee4c2b4e1f625f4742d4fd20164b6e
aivanius <aivanius@yandex.ru>
2025-10-19 19:39:43 +0700

    asggasgas

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactStrictMode: false
+    reactStrictMode: false,
+    eslint: {
+        ignoreDuringBuilds: true, // отключает ESLint при сборке
+    },
+    typescript: {
+        ignoreBuildErrors: true, // отключает ошибки TypeScript при сборке
+    },
 };
 
 export default nextConfig;
